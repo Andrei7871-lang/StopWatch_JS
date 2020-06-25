3722dbc9abe344f142a35e70463098c4e12e8938
Jervx <louellagracechua@gmail.com>
2020-06-25 11:43:13 +0800

    js code jervx added June 28 2020

--- javaScript.js
+++ javaScript.js
@@ -13,3 +13,26 @@ function resume(){
 function reset(){
     //pang reset
 }
+
+function update(){
+    milis += 1;
+    if(milis >= 1000){
+        sec++;
+        milis = 0;
+    }
+
+    if(sec == 60){
+        min++;
+        sec = 0;
+    }
+
+    if(min == 60){
+        hour++;
+        min = 0;
+    }
+    write();
+}
+
+function write(){
+    document.getElementById("time-lbl").innerHTML = hour + " : " + min + " : " +sec+  " : " +milis
+}
